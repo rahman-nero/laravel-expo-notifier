e4447517323fe76f6faba64d63c82db33c222e04
Rahman Dikaev <55330121+rahman-nero@users.noreply.github.com>
2024-07-19 06:12:59 +0300

    Update ExpoTicket.php

--- src/Models/ExpoTicket.php
+++ src/Models/ExpoTicket.php
@@ -10,7 +10,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class ExpoTicket extends Model
 {
     protected $guarded = ['id'];
-
+    
+    protected $connection = 'flex_expo_notification';
+    
     public function notification(): BelongsTo
     {
         return $this->belongsTo(ExpoNotification::class);
